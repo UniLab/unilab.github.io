2f77f2e8c1f42951046612665c4785c810219f11
unnikked <unnikked@gmail.com>
2014-05-17 19:09:43 +0200

    site upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function() {
+    /* Slider di immagini */
+    $("imgSlider").carousel();
+    
+    /* News dal portale UNICAL */
+    $("#news-widget").rssfeed("http://www.unical.it/portale/rss/rss2.0.xml", {
+        dateformat: 'date',
+    }, function(e) {
+        $(e).find("div.rssBody").easyTicker({
+            interval: 4500,
+            visible: 1
+		});
+		
+		$(e).find("a").addClass("text-error");
+		
+    },
+        $("#news-widget").fadeIn("slow")
+    );
+
+    /* News dal Forum */
+    $("#news-content").FeedEk({
+        //FeedUrl : "http://www.uni-lab.eu/vanilla/index.php?p=/categories/novit%C3%A0/feed.rss",
+        FeedUrl : "../feed/master.rss",
+        MaxCount : 3,
+        ShowDesc : true,
+        ShowPubDate: true
+    });
+});
